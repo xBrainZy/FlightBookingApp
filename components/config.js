013765c5ca8810c3d5b64e7c39da5c656ca954db
xBrainZy <cocoacola00729@gmail.com>
2023-11-18 14:07:12 +0300

    added firebase config file

--- components/config.js
+++ components/config.js
@@ -16,4 +16,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
 export const auth = getAuth(app)
-export const storage = getStorage(app);
+export const storage = getStorage(app); 
