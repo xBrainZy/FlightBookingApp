4b37bc0a000fa091fb08ec19f4c7071f5c1a11ad
xBrainZy <cocoacola00729@gmail.com>
2023-11-19 01:18:26 +0300

    Modified

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import LoginScreen from './components/LoginScreen';
 import Home from './components/Home';
-
+import { ScreenStackHeaderBackButtonImage } from 'react-native-screens';
+import { MaterialIcons, MaterialCommunityIcons, FontAwesome} from 'react-native-vector-icons';
 
 
 const Stack = createNativeStackNavigator();
@@ -16,9 +17,19 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Login'>
-        <Stack.Screen name='Login' component={LoginScreen} 
+        <Stack.Screen name='Login' component={LoginScreen} options={{headerShown: false} }
         />
-        <Stack.Screen name='Home' component={Home} 
+        <Stack.Screen name='Home' component={Home} options={{headerStyle: {
+      backgroundColor: '#00D23B',
+      
+    }, 
+           
+          headerTitle: 'Search Flights',
+          headerTitleAlign: 'center',
+          headerTitleStyle: {color: 'white'},
+          headerLeft: ()=> <MaterialIcons name='arrow-back-ios' 
+       onPress={()=> navigation.replace('Login')} size={30} color={'white'}/>
+      }}
         />
 
       </Stack.Navigator>
