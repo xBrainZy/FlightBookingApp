797921a7e7c2fee7b20c3d6538e4eb843591983f
xBrainZy <cocoacola00729@gmail.com>
2023-11-19 22:07:01 +0300

    Added a stack screen navigating to the tab navs

--- App.js
+++ App.js
@@ -3,9 +3,10 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import LoginScreen from './components/LoginScreen';
-import Home from './components/Home';
+
 import { ScreenStackHeaderBackButtonImage } from 'react-native-screens';
 import { MaterialIcons, MaterialCommunityIcons, FontAwesome} from 'react-native-vector-icons';
+import MyTabs from './components/MyTabs';
 
 
 const Stack = createNativeStackNavigator();
@@ -19,17 +20,7 @@ export default function App() {
       <Stack.Navigator initialRouteName='Login'>
         <Stack.Screen name='Login' component={LoginScreen} options={{headerShown: false} }
         />
-        <Stack.Screen name='Home' component={Home} options={{headerStyle: {
-      backgroundColor: '#00D23B',
-      
-    }, 
-           
-          headerTitle: 'Search Flights',
-          headerTitleAlign: 'center',
-          headerTitleStyle: {color: 'white'},
-          headerLeft: ()=> <MaterialIcons name='arrow-back-ios' 
-       onPress={()=> navigation.replace('Login')} size={30} color={'white'}/>
-      }}
+        <Stack.Screen name='Home' component={MyTabs} 
         />
 
       </Stack.Navigator>
