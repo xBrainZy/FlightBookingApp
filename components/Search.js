8bd7d4b623f4030abae194137cf2abeb0692501a
xBrainZy <cocoacola00729@gmail.com>
2023-11-25 12:57:13 +0300

    Added a new file Search

---
+++ components/Search.js
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, {useEffect, useState} from 'react'
+
+const Search = ({navigation, route}) => {
+  return (
+    <View>
+      <Text>Search</Text>
+    </View>
+  )
+}
+
+export default Search
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        //justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#00D23B'
+    },
+})
