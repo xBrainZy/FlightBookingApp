8c1f227a3bb9f74c2975c60e3bcd7cd7bd66b04c
xBrainZy <cocoacola00729@gmail.com>
2023-12-01 18:00:05 +0300

    useState is 1 step behind useEffect

--- components/Bookings.js
+++ components/Bookings.js
@@ -9,84 +9,102 @@ import { MaterialIcons, MaterialCommunityIcons, FontAwesome5} from 'react-native
 
 import {doc, setDoc,getDocs, collection,deleteDoc, getDoc, updateDoc, arrayUnion, query, where, onSnapshot} from "firebase/firestore";
 
-import { db } from './Config'
+import { db } from './config'
 
 const windowHeight = Dimensions.get('window').height 
 const windowWidth = Dimensions.get('window').width
 
 const myFontSize = windowHeight*0.01 + windowWidth*0.05
 
+
 const Bookings = ({navigation, route}) => {
     const [user, setUser] = useState()
     const [x, setX] = useState(false)
-    const [initialRender,setInitialRender] = useState(false)
+    const [initialRender,setInitialRender] = useState(true)
     const[filteredData, setFilteredData] = useState([])
+    const[data, setData] = useState([])
     useEffect(() => {
-        /* const x = async () => {
-            const q = query(collection(db, "Users"), where("signedIn", "==", true));
-            const docs = await getDocs(q);
-            docs.forEach((doc) => {
-                // doc.data() is never undefined for query doc snapshots
-                console.log(doc.id, " => ", doc.data().user);
-                username = doc.data().user
-                setUser(username)
-    
-                });
-        } */
+      /* const x = async () => {
+          const q = query(collection(db, "Users"), where("signedIn", "==", true));
+          const docs = await getDocs(q);
+          docs.forEach((doc) => {
+              // doc.data() is never undefined for query doc snapshots
+              console.log(doc.id, " => ", doc.data().user);
+              username = doc.data().user
+              setUser(username)
+  
+              });
+      } */
 
-    const userCollection = collection(db, 'Users')
+  const userCollection = collection(db, 'Users')
 
-    const unsub2 = onSnapshot(userCollection, (snapshot) => {
-      const userData = snapshot.docs.map((doc) => doc.data());
-      console.log('USERDATA: ', userData);
-      if (userData[0].signedIn == true){
-        setUser(userData[0].user);
+  const unsub2 = onSnapshot(userCollection, (snapshot) => {
+    const userData = snapshot.docs.map((doc) => doc.data());
+    console.log('USERDATA: ', userData);
+    if (userData[0].signedIn == true){
+      setUser(userData[0].user);
+    }
+
+    userData.forEach((x)=>{
+      if (x.signedIn == true){
+        setUser(x.user)
       }
-
-      userData.forEach((x)=>{
-        if (x.signedIn == true){
-          setUser(x.user)
-        }
-      })
     })
+  })
 
-    const collectionRef = collection(db, 'Bookings')
+  const collectionRef = collection(db, 'Bookings')
+  
+  const unsubscribe = onSnapshot(collectionRef, (snapshot) => {
+      const newData = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }))
+
+  //   const newData = snapshot.docs.map((doc) => doc.data());
+  //                       )
+  //                       let temp = []
+  //                     data.forEach(element => {
+  //                     if(element.user == user){
+  //                         temp.push(element) 
+  //                     }\
+
     
-    const unsubscribe = onSnapshot(collectionRef, (snapshot) => {
-        const newData = snapshot.docs.map((doc) => doc.data())
+    setData(newData)
+    console.log(data)
 
-    //   const newData = snapshot.docs.map((doc) => doc.data());
-    //                       )
-    //                       let temp = []
-    //                     data.forEach(element => {
-    //                     if(element.user == user){
-    //                         temp.push(element) 
-    //                     }\
 
-            const filteredData = newData.filter((x) => x.user === user);
-            console.log(filteredData)
-            setFilteredData(filteredData);
+          const filterData = newData.filter((x) => x.user === user);
+          console.log(filterData)
+          setFilteredData(filterData);
+          //setFilteredData(filterData);
 
-            console.log('RENDER 1')
-                 })
-    
-                // if (initialRender){
-                //     console.log('RENDER 1')
-                    
-                // }
-            
-            
-                  return () =>{
-                    unsub2() 
-                      
-                    unsubscribe();
+          console.log('RENDER 1')
+               }) 
+  
+              if (initialRender){
+                   console.log('RENDER 1')
+                   setInitialRender(false)
+                   //setInitialRender(!initialRender)
                    
-                    //setInitialRender(!initialRender)
-                  } 
-        
-    }, [initialRender]
-    )
-    setInitialRender(false)
+                  
+               }
+
+               
+          
+          
+                return () =>{
+                   
+                    
+                  unsubscribe();
+                  unsub2();
+                 
+                  //setInitialRender(!initialRender)
+                } 
+      
+  }, [initialRender]  
+  )
+    
+    //setInitialRender(true)
 
     const delDoc = async (id) =>{
 
@@ -115,7 +133,7 @@ const Bookings = ({navigation, route}) => {
       <SafeAreaView style={{ height: hp(100),}}>
         <View style={{backgroundColor: 'white', margin: wp(5), marginTop: wp(17), borderRadius: wp(6)
         }}>
-            {filteredData. length > 0? filteredData.map((x) => {
+            {filteredData. length > 0 ? filteredData.map((x) => {
                 return(
                     <View style={styles.CardView} key={x.id}>
                     <View>
