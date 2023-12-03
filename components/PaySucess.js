05431be9003892dc695172770503a93affb720ea
xBrainZy <cocoacola00729@gmail.com>
2023-12-04 01:36:51 +0300

    added animation

--- components/PaySucess.js
+++ components/PaySucess.js
@@ -1,4 +1,4 @@
-import { StyleSheet, TextInput, View,TouchableOpacity,Text,KeyboardAvoidingView,Dimensions, SafeAreaView, Button} from 'react-native'
+import { StyleSheet, TextInput, View,TouchableOpacity,Text,KeyboardAvoidingView,Dimensions, SafeAreaView, Button,Animated} from 'react-native'
 import { MaterialIcons, MaterialCommunityIcons, FontAwesome5, AntDesign} from 'react-native-vector-icons';
 import React,{useEffect, useState} from 'react';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
@@ -9,15 +9,59 @@ const windowHeight = Dimensions.get('window').height
 const windowWidth = Dimensions.get('window').width
 
 const myFontSize = windowHeight*0.01 + windowWidth*0.05
-
+const StarAnimation = () => {
+    const [animation] = useState(new Animated.Value(0));
+  
+    useEffect(() => {
+      animateStars();
+    }, []);
+  
+    const animateStars = () => {
+      Animated.loop(
+        Animated.sequence([
+          Animated.timing(animation, {
+            toValue: 1,
+            duration: 1000,
+            useNativeDriver: true,
+          }),
+          Animated.timing(animation, {
+            toValue: 0,
+            duration: 1000,
+            useNativeDriver: true,
+          }),
+        ]),
+      ).start();
+    };
+  
+    const starStyle = {
+      transform: [
+        {
+          translateY: animation.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0, 5], // Adjust the vertical movement as needed
+          }),
+        },
+      ],
+    };
+    return (
+        <Animated.View style={[styles.star, starStyle]}>
+          <AntDesign name={'star'} size={wp(20)} color='white' />
+        </Animated.View>
+      );
+    };
 const PaySucess = ({navigation, route}) => {
     let {id, numOfTravellers} = route.params
     console.log(id)
+    
   return (
     <SafeAreaView style={styles.container}>
+        
         <View style={styles.ball}>
-        <AntDesign name={'check'} size={wp(30)} color='white'/>
+        {/* <AntDesign name={'check'} size={wp(30)} color='white'/> */}
         </View>
+        <View style={styles.starsContainer}>
+        <StarAnimation />
+      </View>
 
         <View style={{alignItems: 'center'}}>
             <Text style={{color: 'white', fontSize: myFontSize * 0.9, margin: wp(3)}}>
@@ -52,6 +96,16 @@ const styles = StyleSheet.create({
         backgroundColor: '#00D23B',
         //padding: wp(15)
     },
+    starsContainer: {
+        position: 'absolute',
+       // top: hp(20),
+        flexDirection: 'row',
+        //backgroundColor:'white'
+      },
+      star: {
+        marginRight: wp(1),
+        marginTop: wp(7)
+      },
 
     ball : {
         height: hp(20),
